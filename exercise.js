a3602a7b795ce430510c6cf430f37d0df1581fb6
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-08-11 18:05:57 -0400

    Day 5 hw

---
+++ exercise.js
@@ -0,0 +1,37 @@
+// Exercise 1
+// The below function does not work. The variable twoPlusTwo gets set to
+// undefined. Refactor the function to make it work.
+
+function addNumbers(numberA, numberB) {
+  return numberA + numberB;
+}
+
+var twoPlusTwo = addNumbers(2, 2)
+
+console.log(twoPlusTwo) // 4
+
+// Exercise 2
+// Write a function called yell that logs out an uppercase version of a string.
+ function yell(x) {
+return x;
+ }
+ var result = "Hello World"
+ console.log(result.toUpperCase());
+// Write a function called yell10 that uses your yell function to log out an
+// uppercase version of a string 10 times.
+  function yell10(x, x){
+return x + x;
+}
+var result ="Hello World"
+console.log(result.toUpperCase());
+
+// Exercise 3
+// Write a function called longest that returns the longest of two input strings
+//or arrays.
+
+
+
+// Exercise 4
+// Write a function called isVowel that takes a character (i.e. a string of
+// length 1) and returns true if it is a vowel, uppercase or lowercase. The
+// function should return false if the character is not a vowel.
